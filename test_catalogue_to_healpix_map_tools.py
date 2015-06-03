ef6e200c9c43015dfde206fe1f19b13a2c511c9d
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-06-03 22:35:09 +0100

    4096 maps creation added

--- test_catalogue_to_healpix_map_tools.py
+++ test_catalogue_to_healpix_map_tools.py
@@ -5,8 +5,9 @@ col_dec = 2
 col_gamma_1 = 4
 col_gamma_2 = 5
 
+n_side = 4096
 #n_side = 2048
-n_side = 1024
+#n_side = 1024
 #n_side = 512
 
 #c2m = catalogue_to_healpix_map_tools.converter(
@@ -21,11 +22,17 @@ c2m.accumulate_objects()
 #c2m.write_maps("/resource/data/MICE/temp_maps/test")
 #c2m.write_png_maps("/resource/data/MICE/temp_maps/test")
 
+c2m.write_maps("/resource/data/MICE/maps/4096/mice_v2_0_shear")
+c2m.write_png_maps("/resource/data/MICE/maps/4096/mice_v2_0_shear")
+
 #c2m.write_maps("/resource/data/MICE/maps/2048/mice_v2_0_shear")
 #c2m.write_png_maps("/resource/data/MICE/maps/2048/mice_v2_0_shear")
+
+#c2m.write_maps("/resource/data/MICE/maps/1024/mice_v2_0_shear")
+#c2m.write_png_maps("/resource/data/MICE/maps/1024/mice_v2_0_shear")
 
 #c2m.write_maps("/resource/data/MICE/maps/512/mice_v2_0_shear")
 #c2m.write_png_maps("/resource/data/MICE/maps/512/mice_v2_0_shear")
 
-c2m.write_maps("/resource/data/MICE/maps/1024/mice_v2_0_shear")
-c2m.write_png_maps("/resource/data/MICE/maps/1024/mice_v2_0_shear")
+
+
