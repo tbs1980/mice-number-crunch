6b898fdf118fc9d82be4dcb0d77d2c8f8e1eeb8a
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-09-30 18:31:59 +0100

    changed varaiables for splinter

--- test_euclid_sim_data.py
+++ test_euclid_sim_data.py
@@ -7,13 +7,13 @@ col_gamma_1 = 3
 col_gamma_2 = 4
 
 n_side = 256
-file_name = '/Users/sbalan/Projects/MICE_EUCLID/euclidcat01_100k.dat'
+file_name = '/share/data1/sbalan/EUCLID/henrique/toSree01/euclidcat01.dat'
 z_bounds = [0.55,0.65]
 
 c2m = catalogue_to_healpix_map_tools.converter(file_name,col_ra,col_dec,col_z,col_gamma_1,col_gamma_2,n_side,z_bounds)
 
 c2m.accumulate_objects()
 
-c2m.write_maps("/Users/sbalan/Projects/MICE_EUCLID/euclid_sim")
-c2m.write_png_maps("/Users/sbalan/Projects/MICE_EUCLID/euclid_sim")
+c2m.write_maps('/share/data1/sbalan/EUCLID/henrique/process_data/maps/ns_256')
+c2m.write_png_maps('/share/data1/sbalan/EUCLID/henrique/process_data/maps/ns_256')
 
