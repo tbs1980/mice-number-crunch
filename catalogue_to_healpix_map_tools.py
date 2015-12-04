81b581340755d152b7263c137e3b8522ce9692f0
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-12-04 10:16:53 +0000

    removed the test map check in accumulate

--- catalogue_to_healpix_map_tools.py
+++ catalogue_to_healpix_map_tools.py
@@ -185,14 +185,16 @@ class converter:
 
                     try:
                         pix = hp.ang2pix(self.n_side,theta,phi)
+                        
                         #print "pix = ",pix,"test map val= ",self.test_map[pix], "G1 Val = ",G1_val
                         #print pix,G1_val , test_map[pix]
                         #print ""
-                        if self.test_map_file_name != None :
+                        #if self.test_map_file_name != None :
                             #print G1_val , self.test_map[pix]
-                            if abs(G1_val - self.test_map[pix]) > 1e-5  :
+                        #    if abs(G1_val - self.test_map[pix]) > 1e-5  :
                                 #raise ValueError("Value of G1 obtained from catalogue "+str(G1_val)+", does not match the one from test_map "+str(test_map[pix]))
-                                print "Value of G1 obtained from catalogue "+str(G1_val)+", does not match the one from test_map "+str(test_map[pix])
+                        #        print "Value of G1 obtained from catalogue "+str(G1_val)+", does not match the one from test_map "+str(test_map[pix])
+
                         if self.mask[pix] > 0.:
                             #print "we have an observed pixel"
                             # increase the object count in the pixel
